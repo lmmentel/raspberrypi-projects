1e6bcd80be9b49e56232a4f68a83c5ce31f4fae1
Lukasz Mentel <lmmentel@gmail.com>
2018-08-13 01:05:44 +0200

    version bump

--- conductor/run_influxdb.sh
+++ conductor/run_influxdb.sh
@@ -3,7 +3,7 @@
 docker run -p 8086:8086 -p 8083:8083 \
     -v $PWD/influxdb:/var/lib/influxdb \
     -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
-    influxdb -config /etc/influxdb/influxdb.conf
+    influxdb:1.6.1 -config /etc/influxdb/influxdb.conf
 
 
 # docker run -p 8086:8086 \
