504c7edf41a332e9a55d05be136a3648fb24623f
Lukasz Mentel <lmmentel@gmail.com>
2018-08-12 20:38:00 +0200

    add function to insert a row to db

--- bme280/create_sqlite_db.py
+++ bme280/create_sqlite_db.py
@@ -4,13 +4,33 @@ from sqlalchemy import Table, Column, Float, String, MetaData, DateTime
 
 metadata = MetaData()
 
-tsdta = Table('sensor_data', metadata,
-              Column('timestamp', DateTime, primary_key=True),
-              Column('temperature', Float),
-              Column('pressure', Float),
-              Column('humidity', Float)
-             )
+sensor_data = Table('sensor_data', metadata,
+                    Column('timestamp', DateTime, primary_key=True),
+                    Column('temperature', Float),
+                    Column('pressure', Float),
+                    Column('humidity', Float),
+                    Column('light_dep_resistance', Float)
+                    )
 
-engine = create_engine('sqlite:///rpisensor.db', debug=True)
 
-metadata.create_all(engine)
+def create_db(db_path):
+
+    engine = create_engine('sqlite:///{}'.format(db_path), debug=True)
+    metadata.create_all(engine)
+
+
+def insert_row(db_path, sensor, ts, temp, pressure, humidity, ldr):
+    '''
+    Save to the db
+    '''
+
+    engine = create_engine('sqlite:///{}'.format(db_path),
+                           echo=False)
+
+    with engine.begin() as conn:
+        conn.execute(sensor_data.insert(),
+                     timestamp=ts,
+                     temperature=temp,
+                     pressure=pressure,
+                     humidity=humidity,
+                     light_dep_resistance=ldr)
