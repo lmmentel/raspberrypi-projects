0615bc576a75e77b4f8331b47129caa54f4b6052
Lukasz Mentel <lmmentel@gmail.com>
2018-08-12 20:51:53 +0200

    separate ldr from bme280

--- ldr/ldr.py
+++ ldr/ldr.py
@@ -1,36 +1,14 @@
+
 from __future__ import print_function
 from datetime import datetime
 from time import sleep, time
 
 import RPi.GPIO as GPIO
-from Adafruit_BME280 import *
 
 
 GPIO.setmode(GPIO.BOARD)
 DT_MFT = 'YYYY-MM-DD HH:mm:SS'
 FREQ = 60   # frequency of data collection in sec
-
-
-def measure_tph(sensor):
-    '''
-    Measure temperature, pressure and humidity with BME280 sensor
-
-    Args:
-        sensor
-
-    Returns:
-        tuple:
-            - temperature in degrees C
-            - pressure in kPa
-            - realtive humidity in %
-    '''
-
-    degrees = sensor.read_temperature()
-    pascals = sensor.read_pressure()
-    pressure = pascals / 100
-    humidity = sensor.read_humidity()
-
-    return degrees, pressure, humidity
 
 
 def measure_ldr_resistance(pin_no, capacitance=0.000001):
@@ -62,20 +40,17 @@ def measure_ldr_resistance(pin_no, capacitance=0.000001):
 
 if __name__ == '__main__':
 
-    sensor = BME280(t_mode=BME280_OSAMPLE_8,
-                    p_mode=BME280_OSAMPLE_8,
-                    h_mode=BME280_OSAMPLE_8)
+    # define pin that goes to the LDR circuit
+    ldr_pin_no = 7
 
     while True:
         try:
             ts = datetime.now()
-            temp, pressure, humidity = measure_tph(sensor)
+            ldr_res = measure_ldr_resistance(ldr_pin_no)
 
             print(' | '.join([
                   ts.isoformat(),
-                  '{0:0.3f} deg C'.format(temp),
-                  '{0:0.2f} hPa'.format(pressure),
-                  '{0:0.2f} %'.format(humidity)
+                  '{0:0.2f} Ohm'.format(ldr_res)
                   ]))
 
             sleep(FREQ)
