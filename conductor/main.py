6f15a63817b223bacfff9457368f352a94a8272d
Lukasz Mentel <lmmentel@gmail.com>
2018-08-19 17:30:19 +0200

    add ldr reading

--- conductor/main.py
+++ conductor/main.py
@@ -1,8 +1,8 @@
 
 from datetime import datetime
-from time import sleep
 from influxdb import InfluxDBClient
 import requests
+import time
 import toml
 
 import RPi.GPIO as GPIO
@@ -41,10 +41,39 @@ def measure_tph(sensor):
     return degrees, pressure, humidity
 
 
+def measure_ldr_resistance(pin_no, capacitance=0.000001):
+    '''
+    Calculate the resistance from the measured
+    capacitor charging time and LDR
+
+    Args:
+        pin_no (int): pin numer
+        capacitance (float): capacitance
+    '''
+
+    # output on the pin for
+    GPIO.setup(pin_no, GPIO.OUT)
+    GPIO.setup(pin_no, GPIO.LOW)
+    time.sleep(0.1)
+
+    # change the pin back to input
+    GPIO.setup(pin_no, GPIO.IN)
+
+    # count until pin goes to HIGH
+    start = time.time()
+    end = time.time()
+    while (GPIO.input(pin_no) == GPIO.LOW):
+        end = time.time()
+
+    return (end - start) / capacitance
+
+
 if __name__ == '__main__':
 
     print('started: ', datetime.now().isoformat())
 
+    # define pin that goes to the LDR circuit
+    ldr_pin_no = 7
 
     sensor = BME280(t_mode=BME280_OSAMPLE_8,
                     p_mode=BME280_OSAMPLE_8,
@@ -56,12 +85,14 @@ if __name__ == '__main__':
         try:
             ts = datetime.now()
             temp, pressure, humidity = measure_tph(sensor)
+            ldr_res = measure_ldr_resistance(ldr_pin_no)
 
             print(' | '.join([
                   ts.isoformat(),
                   '{0:0.3f} deg C'.format(temp),
                   '{0:0.2f} hPa'.format(pressure),
-                  '{0:0.2f} %'.format(humidity)
+                  '{0:0.2f} %'.format(humidity),
+                  '{0:0.2f} Ohm'.format(ldr_res)
                   ]))
 
             data = {
@@ -71,6 +102,7 @@ if __name__ == '__main__':
                     "temperature": temp,
                     "pressure": pressure,
                     "humidity": humidity,
+                    "ldr": ldr_res,
                 },
             }
 
@@ -78,7 +110,7 @@ if __name__ == '__main__':
 
             # ping healthcheck
             requests.get(config['healthcheck']['url'])
-            sleep(FREQ)
+            time.sleep(FREQ)
 
         except KeyboardInterrupt:
             print('bye bye...')
