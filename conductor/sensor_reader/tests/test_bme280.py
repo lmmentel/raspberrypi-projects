c486447e80f7c405e9570dbbd514b02c5ed9cffa
Lukasz Mentel <lmmentel@gmail.com>
2018-12-28 14:43:38 +0100

    add basic tests

---
+++ conductor/sensor_reader/tests/test_bme280.py
@@ -0,0 +1,74 @@
+
+from datetime import datetime
+from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBServerError
+import requests
+import time
+import toml
+
+import RPi.GPIO as GPIO
+from Adafruit_BME280 import *
+
+
+GPIO.setmode(GPIO.BOARD)
+DT_MFT = 'YYYY-MM-DD HH:mm:SS'
+FREQ = 5   # frequency of data collection in sec
+
+
+
+def measure_tph(sensor):
+    '''
+    Measure temperature, pressure and humidity with BME280 sensor
+
+    Args:
+        sensor
+
+    Returns:
+        tuple:
+            - temperature in degrees C
+            - pressure in kPa
+            - realtive humidity in %
+    '''
+
+    degrees = sensor.read_temperature()
+    pascals = sensor.read_pressure()
+    pressure = pascals / 100
+    humidity = sensor.read_humidity()
+
+    return degrees, pressure, humidity
+
+
+
+if __name__ == '__main__':
+
+    print('started: ', datetime.now().isoformat())
+
+    # define pin that goes to the LDR circuit
+
+    sensor = BME280(t_mode=BME280_OSAMPLE_8,
+                    p_mode=BME280_OSAMPLE_8,
+                    h_mode=BME280_OSAMPLE_8)
+
+    while True:
+        try:
+            ts = datetime.now()
+            temp, pressure, humidity = measure_tph(sensor)
+
+            print(' | '.join([
+                  ts.isoformat(),
+                  '{0:0.3f} deg C'.format(temp),
+                  '{0:0.2f} hPa'.format(pressure),
+                  '{0:0.2f} %'.format(humidity),
+                  ]))
+
+            time.sleep(FREQ)
+
+        except KeyboardInterrupt:
+            print('bye bye...')
+            break
+        except:
+            raise
+            break
+
+    # cleanup
+    GPIO.cleanup()
