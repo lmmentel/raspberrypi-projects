d7563dae7638c3b1168dcbe9adb76d9028bf9c62
Lukasz Mentel <lmmentel@gmail.com>
2019-11-14 22:27:06 +0100

    printing to logging update

--- arduino/catch_serial.py
+++ arduino/catch_serial.py
@@ -1,11 +1,21 @@
 
 from datetime import datetime
 import csv
+import logging
 import serial
 
 
 ARDUINO_PORT = "/dev/ttyUSB0"
 BAUDRATE = 9600
+
+logger = logging.getLogger('catch_serial_arudino')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 def parse_serial(string):
     '''
@@ -32,12 +42,11 @@ def main():
         try:
             line_raw = ser.readline()
             line = line_raw.decode("utf-8")
-            print(line, end='')
             parsed = parse_serial(line)
-            print(parsed)
+            logger.info(str(parsed))
             with open('arduino_data.csv', 'a') as fo:
                 writer = csv.DictWriter(fo, fieldnames=['DT', 'LDR', 'MQ3', 'MQ9'], delimiter=',')
-                writer.writerow({'DT':datetime.now(), **parsed})
+                writer.writerow({'DT': datetime.now(), **parsed})
         except KeyboardInterrupt:
             print("see you")
             break
