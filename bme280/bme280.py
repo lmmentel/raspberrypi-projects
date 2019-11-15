417737397d05e87ac5633c4e4b596b364c951f85
Lukasz Mentel <lmmentel@gmail.com>
2019-11-15 23:37:03 +0100

    print to logging

--- bme280/bme280.py
+++ bme280/bme280.py
@@ -1,8 +1,19 @@
+import logging
+
 import board
 import digitalio
 import busio
 import time
 import adafruit_bme280
+
+logger = logging.getLogger('bme280')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -17,8 +28,15 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 bme280.sea_level_pressure = 1013.25
 
 while True:
-    print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.humidity)
-    print("Pressure: %0.1f hPa" % bme280.pressure)
-    print("Altitude = %0.2f meters" % bme280.altitude)
+    #print("\nTemperature: %0.1f C" % bme280.temperature)
+    #print("Humidity: %0.1f %%" % bme280.humidity)
+    #print("Pressure: %0.1f hPa" % bme280.pressure)
+    #print("Altitude = %0.2f meters" % bme280.altitude)
     time.sleep(2)
+
+    values = {
+        'bme280.temperature': bme280.temperature,
+        'bme280.humidity': bme280.humidity,
+        'bme280.pressure': bme280.pressure
+    }
+    logger.info(str(values))
