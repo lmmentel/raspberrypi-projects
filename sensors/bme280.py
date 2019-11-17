295812165a761b2ed7ec617282e612450d3fe638
Lukasz Mentel <lmmentel@gmail.com>
2019-11-17 15:20:55 +0100

    rename get to read_bme280

--- sensors/bme280.py
+++ sensors/bme280.py
@@ -18,16 +18,10 @@ logger.addHandler(ch)
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-
-# OR create library object using our Bus SPI port
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#bme_cs = digitalio.DigitalInOut(board.D10)
-#bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
-
-# change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
 
-def get_bme280():
+
+def read_bme280():
     '''
     Read the temperature humidity and pressure from the BME280
     and return a dict with the values
@@ -40,6 +34,7 @@ def get_bme280():
     }
 
     return values
+
 
 if __name__ == '__main__':
 
