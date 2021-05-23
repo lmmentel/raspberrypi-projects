9a6a11ab6b59586609c7393f624923c6d9656dcc
Lukasz Mentel <lmmentel@gmail.com>
2021-05-23 22:32:43 +0200

    add hostname

--- pi/monitor.py
+++ pi/monitor.py
@@ -1,5 +1,6 @@
 
 import re
+import socket
 from subprocess import run
 from time import sleep
 
@@ -24,39 +25,42 @@ def get_gpu_temp():
         raise ValueError('no match found in: '.format(string))
 
 
-def read_metrics():
+def read_metrics(host):
     '''
     Collect metric about the raspberry pi and return a dict
     '''
+
 
     metrics = dict()
 
     # CPU usage
     cpu = psutil.cpu_percent(interval=1, percpu=False)
-    metrics['device.cpu_percent_used'] = cpu
+    metrics[f'{host}.cpu_percent_used'] = cpu
     # print(psutil.getloadavg())
     # print(psutil.cpu_freq())
     # memory usage
     mem = psutil.virtual_memory()
-    metrics['device.mem_percent_used'] = mem.percent
-    metrics['device.mem_available'] = bytes2human(mem.available)
+    metrics[f'{host}.mem_percent_used'] = mem.percent
+    metrics[f'{host}.mem_available'] = bytes2human(mem.available)
     # disk usage
     disk = psutil.disk_usage('/')
-    metrics['device.disk_percent_used'] = disk.percent
-    metrics['device.disk_free'] = bytes2human(disk.free)
+    metrics[f'{host}.disk_percent_used'] = disk.percent
+    metrics[f'{host}.disk_free'] = bytes2human(disk.free)
     # CPU temperature
     temp = psutil.sensors_temperatures(fahrenheit=False)
     #metrics['cpu_temp_c'] = temp['cpu-thermal'][0].current
     # GPU temperature
     gpu = get_gpu_temp()
-    metrics['device.gpu_temp_c'] = gpu
+    metrics[f'{host}.gpu_temp_c'] = gpu
 
     return metrics
 
 if __name__ == '__main__':
 
+    hostname = socket.gethostname()
+
     while True:
-        metrics = get_metrics()
+        metrics = get_metrics(hostname)
         print(tabulate([metrics],  tablefmt='plain',
                 floatfmt=".2f", headers='keys'))
         sleep(1)
